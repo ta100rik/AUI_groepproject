7eaceb08f00573f081ab94a63a04343416d6dcd8
zyad osseyran <zyadosseyran@gmail.com>
2019-03-13 19:20:53 +0100

    select box

--- src/Gerechtbeheersysteem-app/content-app.js
+++ src/Gerechtbeheersysteem-app/content-app.js
@@ -1,9 +1,13 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@polymer/paper-input/paper-input.js';
+import 'high-select/lib/high-select.js';
+
 /**
  * @customElement
  * @polymer
  */
+
+
 class ContentApp extends PolymerElement {
   static get template() {
     return html`
@@ -22,8 +26,13 @@ class ContentApp extends PolymerElement {
        
       </style>
      <div class="content">
-       Tafelnummer: <paper-input always-float-label value="{{prop1}}"></paper-input>
-       <div>[[prop1]]</div>
+        Tafelnummer: <paper-input always-float-label value="{{prop1}}"></paper-input>
+        <div>[[prop1]]</div>
+        <high-select search>
+          <template is="dom-repeat" items="{{Gerechten}}" as="gerecht">
+            <high-option> [[gerecht]]</high-option> 
+          </template>
+        </high-select>
       </div>
      
     `;
@@ -32,6 +41,14 @@ class ContentApp extends PolymerElement {
     return {
       prop1: {
         type: String
+      },
+      Gerechten:{
+        type:Array,
+         value: [
+          "Bai pangang",
+          "Witte rijst",
+          "Bami met ei"
+        ]
       }
     };
   }
